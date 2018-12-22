692fb36bbc90654492f3c2365954fc9e20974fcb
JinQing Lee <hourout@163.com>
2018-12-22 14:53:43 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from io import open
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+setup(name='tensordata',
+      version='0.0.0',
+      install_requires=['pandas', 'numpy', 'imageio', 'tensorflow>=1.12.0'],
+      description='CV, NLP, DM datasets Toolkit for Machine Learning.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/Hourout/tensordata',
+      author='JinQing Lee',
+      author_email='hourout@163.com',
+      keywords=['computer-vision', 'NLP', 'DM', 'Machine Learning', 'datasets'],
+      license='Apache License Version 2.0',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
+      ],
+      packages=find_packages(),
+      zip_safe=False)
