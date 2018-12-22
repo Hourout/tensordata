c38086e4b55b767959e39c59f265c63db12d5162
JinQing Lee <hourout@163.com>
2018-12-22 14:44:47 +0800

    Create __init__.py

---
+++ tensordata/cv/__init__.py
@@ -0,0 +1,7 @@
+from tensordata.cv._cifar import cifar10, cifar100
+
+from tensordata.cv._mnist import mnist, mnist_fashion
+
+from tensordata.cv._mnist_kuzushiji import mnist_kuzushiji10, mnist_kuzushiji49
+
+from tensordata.cv._mnist_tibetan import mnist_tibetan
