9fa6bcd1f4868215ed65e0ed3604012c959f2a9f
JinQing Lee <hourout@163.com>
2018-12-24 18:57:55 +0800

    Create __init__.py

---
+++ tensordata/utils/compress/__init__.py
@@ -0,0 +1,6 @@
+from tensordata.utils.conmpress._zips import zip_files
+from tensordata.utils.conmpress._zips import zip_folder
+from tensordata.utils.conmpress._zips import un_gz
+from tensordata.utils.conmpress._zips import un_tar
+from tensordata.utils.conmpress._zips import un_zip
+from tensordata.utils.conmpress._zips import un_rar
