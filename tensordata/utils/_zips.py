23579505eb3a755950c50fb00cda3c3cc4a72371
JinQing Lee <hourout@163.com>
2018-12-24 17:03:19 +0800

    Update _zips.py

--- tensordata/utils/_zips.py
+++ tensordata/utils/_zips.py
@@ -1,8 +1,9 @@
+import os
 import zipfile
 import tensorflow as tf
 
 def zip_files(files, zip_name):
-    """Compression files
+    """Compression files.
     
     Args:
         files: str or list
@@ -12,9 +13,9 @@ def zip_files(files, zip_name):
     Return:
         zip_name: str, compression files name.
     """
-    assert isinstance(zip_name, str), 'zip_name should be str.'
+    assert isinstance(zip_name, str), '`zip_name` should be str.'
     if isinstance(files, str):
-        assert not tf.gfile.IsDirectory(files), 'Files should be file path.'
+        assert not tf.gfile.IsDirectory(files), '`files` should be file path.'
         with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as z:
             z.write(files)
     elif isinstance(files, list):
@@ -23,5 +24,23 @@ def zip_files(files, zip_name):
                 assert not tf.gfile.IsDirectory(file), 'Elements in the list should be file path.'
                 z.write(file)
     else:
-        raise ValueError('Files should be type of str or list.')
+        raise ValueError('`files` should be type of str or list.')
+    return zip_name
+
+def zip_folder(folder, zip_name):
+    """Compress all files in the folder.
+    
+    Args:
+        folder: str, folder should be folder path.
+        zip_name: str, compression files name.
+    Return:
+        zip_name: str, compression files name.
+    """
+    assert tf.gfile.IsDirectory(folder), '`folder` should be folder path.'
+    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as z:
+        for dirpath, dirnames, filenames in os.walk(folder):
+            fpath = dirpath.replace(startdir, '')
+            fpath = fpath and fpath + os.sep or ''
+            for filename in filenames:
+                z.write(os.path.join(dirpath, filename), fpath+filename)
     return zip_name
