ef9914897236bb4101cbe4085c8d06ae8119cd3f
JinQing Lee <hourout@163.com>
2018-12-25 16:18:20 +0800

    Update and rename _chinese_takeaway_reviews.py to _chinese_reviews_takeaway.py

--- tensordata/nlp/_chinese_reviews_takeaway.py
+++ tensordata/nlp/_chinese_reviews_takeaway.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 import tensorflow as tf
 
-def chinese_takeaway_reviews(root):
+def chinese_reviews_takeaway(root):
     """Chinese takeaway reviews datasets.
     
     datasets url:`https://github.com/SophonPlus/ChineseNlpCorpus/blob/master/datasets/waimai_10k/waimai_10k.csv`
@@ -16,30 +16,30 @@ def chinese_takeaway_reviews(root):
     
     Data storage directory:
     root = `/user/.../mydata`
-    chinese_takeaway_reviews data: 
-    `root/chinese_takeaway_reviews/chinese_takeaway_reviews.json`
-    `root/chinese_takeaway_reviews/chinese_takeaway_reviews.txt`
+    chinese_reviews_takeaway data: 
+    `root/chinese_reviews_takeaway/chinese_reviews_takeaway.json`
+    `root/chinese_reviews_takeaway/chinese_reviews_takeaway.txt`
     Args:
         root: str, Store the absolute path of the data directory.
-              example:if you want data path is `/user/.../mydata/chinese_takeaway_reviews`,
+              example:if you want data path is `/user/.../mydata/chinese_reviews_takeaway`,
               root should be `/user/.../mydata`.
     Returns:
-        Store the absolute path of the data directory, is `root/chinese_takeaway_reviews`.
+        Store the absolute path of the data directory, is `root/chinese_reviews_takeaway`.
     """
     start = time.time()
     assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-    task_path = os.path.join(root, 'chinese_takeaway_reviews')
+    task_path = os.path.join(root, 'chinese_reviews_takeaway')
     if tf.gfile.Exists(task_path):
         tf.gfile.DeleteRecursively(task_path)
     tf.gfile.MakeDirs(task_path)
-    url_json = 'https://raw.githubusercontent.com/Hourout/datasets/master/nlp/chinese_takeaway_reviews/chinese_takeaway_reviews.json'
+    url_json = 'https://raw.githubusercontent.com/Hourout/datasets/master/nlp/chinese_reviews_takeaway/chinese_reviews_takeaway.json'
     url_txt = 'https://raw.githubusercontent.com/SophonPlus/ChineseNlpCorpus/master/datasets/waimai_10k/waimai_10k.csv'
     s = requests.get(url_json)
-    with open(os.path.join(task_path, 'chinese_takeaway_reviews.json'), 'w') as outfile:
+    with open(os.path.join(task_path, 'chinese_reviews_takeaway.json'), 'w') as outfile:
         json.dump(s.json(), outfile, ensure_ascii=False)
         outfile.write('\n')
     s = requests.get(url_txt).content
     data = pd.read_csv(io.StringIO(s.decode('utf-8')))
-    data.to_csv(os.path.join(task_path, 'chinese_takeaway_reviews.txt'), index=False)
-    print('chinese_takeaway_reviews dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    data.to_csv(os.path.join(task_path, 'chinese_reviews_takeaway.txt'), index=False)
+    print('chinese_reviews_takeaway dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
