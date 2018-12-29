115595e50753ecda3c5e0910ddf40f414550b2dd
JinQing Lee <hourout@163.com>
2018-12-29 17:02:41 +0800

    Update __init__.py

--- tensordata/utils/compress/__init__.py
+++ tensordata/utils/compress/__init__.py
@@ -1,5 +1,5 @@
-from tensordata.utils.compress._zips import zip_files
-from tensordata.utils.compress._zips import zip_folder
+from tensordata.utils.compress._zips import files_zip
+from tensordata.utils.compress._zips import folder_zip
 from tensordata.utils.compress._zips import un_gz
 from tensordata.utils.compress._zips import un_tar
 from tensordata.utils.compress._zips import un_zip
