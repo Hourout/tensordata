496ed7d2e370ae8a1f47d669252e2693fe8b0345
JinQing Lee <hourout@163.com>
2018-12-29 16:58:32 +0800

    Update __init__.py

--- tensordata/__init__.py
+++ tensordata/__init__.py
@@ -2,6 +2,7 @@ from tensordata import cv
 from tensordata import nlp
 from tensordata import dm
 from tensordata import utils
+from rensordata import paper
 
 __version__ = '0.0.1'
 __author__ = 'JinQing Lee'
