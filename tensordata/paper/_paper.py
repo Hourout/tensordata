0d19c51130f9ad1694744981db8b754c0e5fec05
JinQing Lee <hourout@163.com>
2018-12-31 15:09:09 +0800

    Update _paper.py

--- tensordata/paper/_paper.py
+++ tensordata/paper/_paper.py
@@ -1,6 +1,13 @@
 import os
+import io
 import time
+import requests
+import concurrent
+import pandas as pd
 import tensorflow as tf
+
+def _download(path):
+    tf.keras.utils.get_file(os.path.join(path.split('|')[0], path.split('|')[1].split('/')[-1]), path.split('|')[1])
 
 def get_arxiv(root, ids, new_name=None):
     """Download paper from https://arxiv.org, the file format is pdf.
@@ -33,4 +40,33 @@ def get_arxiv(root, ids, new_name=None):
     url = 'https://arxiv.org/pdf/'+str(ids)+'.pdf'
     tf.keras.utils.get_file(task_path, url)
     print('arxiv paper download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return task_path
+
+def get_cvpr2018(root):
+    """cvpr2018 datasets from http://openaccess.thecvf.com/CVPR2018.py.
+        
+    cvpr2018 datasets includes 979 papers.
+    
+    Data storage directory:
+    root = `/user/.../mydata`
+    cvpr2018 data: 
+    `root/cvpr2018/...`
+    Args:
+        root: str, Store the absolute path of the data directory.
+              example:if you want data path is `/user/.../mydata/cvpr2018`,
+              root should be `/user/.../mydata`.
+    Returns:
+        Store the absolute path of the data directory, is `root/cvpr2018`.
+    """
+    start = time.time()
+    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
+    task_path = os.path.join(root, 'cvpr2018')
+    if tf.gfile.Exists(task_path):
+        tf.gfile.DeleteRecursively(task_path)
+    tf.gfile.MakeDirs(task_path)
+    url = 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr2018/cvpr2018.txt'
+    data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)[0].tolist()
+    with concurrent.futures.ProcessPoolExecutor() as excutor:
+        excutor.map(_download, [task_path+'|'+i for i in data])
+    print('cvpr2018 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
