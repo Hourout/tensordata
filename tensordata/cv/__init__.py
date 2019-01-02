736334a6576d0edd625090418052750ee2538e92
JinQing Lee <hourout@163.com>
2019-01-02 16:15:22 +0800

    Update __init__.py

--- tensordata/cv/__init__.py
+++ tensordata/cv/__init__.py
@@ -1,7 +1,5 @@
-from tensordata.cv._cifar import cifar10, cifar100
-
-from tensordata.cv._mnist import mnist, mnist_fashion
-
-from tensordata.cv._mnist_kuzushiji import mnist_kuzushiji10, mnist_kuzushiji49
-
-from tensordata.cv._mnist_tibetan import mnist_tibetan
+from tensordata.cv._cifar import *
+from tensordata.cv._mnist import *
+from tensordata.cv._mnist_kuzushiji import *
+from tensordata.cv._mnist_tibetan import *
+from tensordata.cv._coil import *
