3ee271daf0f032a8cbc30a2046207e875e1fabc2
JinQing Lee <hourout@163.com>
2019-01-04 18:52:20 +0800

    Update __init__.py

--- tensordata/utils/compress/__init__.py
+++ tensordata/utils/compress/__init__.py
@@ -1,3 +1,3 @@
-from tensordata.utils.compress._file_compress import *
+from tensordata.utils.compress._files_compress import *
 from tensordata.utils.compress._folder_compress import *
 from tensordata.utils.compress._un_compress import *
