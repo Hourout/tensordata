920c806fba9e2bd184a2716727e9f2231750b45f
JinQing Lee <hourout@163.com>
2019-01-04 14:36:59 +0800

    Update _literature.py

--- tensordata/nlp/chinese/literature/_literature.py
+++ tensordata/nlp/chinese/literature/_literature.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-__all__ = ['shijing', 'youmengying']
+__all__ = ['shijing', 'youmengying', 'huajianji', 'nantang_erzhu_poetry']
 
 def shijing(root):
     """Shijing dataset from Chinese classical literature.
@@ -64,4 +64,69 @@ def youmengying(root):
     url = "https://raw.githubusercontent.com/Hourout/datasets/master/nlp/wenxue/youmengying.json"
     tf.keras.utils.get_file(os.path.join(task_path, url.split('/')[-1]), url)
     print('youmengying dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return task_path
+
+def huajianji(root):
+    """Huajianji dataset from Chinese classical literature.
+    
+    "Hua Jian Ji" is a collection of Chinese poetry 
+    compiled during the Five Dynasties and Ten Kingdoms period. 
+    It is also the first collection of literati in the history of literature. 
+    It was edited by Zhao Chongxi, a later monk. 
+    The book contains 18 classic works of poetry by Wen Tingjun and Wei Zhuang. 
+    It concentrates and typically reflects the subject orientation, 
+    aesthetic taste, physical style and artistic achievement 
+    of the human creation in the early word history.
+    
+    Data storage directory:
+    root = `/user/.../mydata`
+    huajianji data: 
+    `root/huajianji/huajianji.json`
+    Args:
+        root: str, Store the absolute path of the data directory.
+              example:if you want data path is `/user/.../mydata/huajianji`,
+              root should be `/user/.../mydata`.
+    Returns:
+        Store the absolute path of the data directory, is `root/huajianji`.
+    """
+    start = time.time()
+    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
+    task_path = os.path.join(root, 'huajianji')
+    if tf.gfile.Exists(task_path):
+        tf.gfile.DeleteRecursively(task_path)
+    tf.gfile.MakeDirs(task_path)
+    url = "https://raw.githubusercontent.com/Hourout/datasets/master/nlp/wenxue/huajianji.json"
+    tf.keras.utils.get_file(os.path.join(task_path, url.split('/')[-1]), url)
+    print('huajianji dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return task_path
+
+def nantang_erzhu_poetry(root):
+    """Nantang_erzhu_poetry dataset from Chinese classical literature.
+    
+    "The Southern Tang Dynasty's two main words", 
+    is the Southern Tang Dynasty master Li Jing, the latter master Li Yu. 
+    The book was written in the Southern Song Dynasty, 
+    and later generations have been compiled, 
+    and later generations have written various versions.
+    
+    Data storage directory:
+    root = `/user/.../mydata`
+    nantang_erzhu_poetry data: 
+    `root/nantang_erzhu_poetry/nantang_erzhu_poetry.json`
+    Args:
+        root: str, Store the absolute path of the data directory.
+              example:if you want data path is `/user/.../mydata/nantang_erzhu_poetry`,
+              root should be `/user/.../mydata`.
+    Returns:
+        Store the absolute path of the data directory, is `root/nantang_erzhu_poetry`.
+    """
+    start = time.time()
+    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
+    task_path = os.path.join(root, 'nantang_erzhu_poetry')
+    if tf.gfile.Exists(task_path):
+        tf.gfile.DeleteRecursively(task_path)
+    tf.gfile.MakeDirs(task_path)
+    url = "https://raw.githubusercontent.com/Hourout/datasets/master/nlp/wenxue/nantang_erzhu_poetry.json"
+    tf.keras.utils.get_file(os.path.join(task_path, url.split('/')[-1]), url)
+    print('nantang_erzhu_poetry dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
