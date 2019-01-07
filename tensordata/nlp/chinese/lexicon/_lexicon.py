788254e2463fb21e0a33d232b433486839c5e304
JinQing Lee <hourout@163.com>
2019-01-07 16:20:42 +0800

    Update _lexicon.py

--- tensordata/nlp/chinese/lexicon/_lexicon.py
+++ tensordata/nlp/chinese/lexicon/_lexicon.py
@@ -17,7 +17,7 @@ def it(root):
     
     Data storage directory:
     root = `/user/.../mydata`
-    chinese_reviews_ctrip_hotel data: 
+    chinese_lexicon_it data: 
     `root/chinese_lexicon_it/chinese_lexicon_it.txt`
     Args:
         root: str, Store the absolute path of the data directory.
@@ -36,4 +36,94 @@ def it(root):
     data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)
     data.to_csv(os.path.join(task_path, 'chinese_lexicon_it.txt'), index=False, header=None)
     print('chinese_lexicon_it dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return task_path
+
+def animal(root):
+    """Chinese lexicon animal datasets.
+    
+    datasets url:`https://github.com/fighting41love/funNLP`
+    
+    chinese_lexicon_animal dataset contains 17200+ samples.
+    
+    Data storage directory:
+    root = `/user/.../mydata`
+    chinese_lexicon_animal data: 
+    `root/chinese_lexicon_animal/chinese_lexicon_animal.txt`
+    Args:
+        root: str, Store the absolute path of the data directory.
+              example:if you want data path is `/user/.../mydata/chinese_lexicon_animal`,
+              root should be `/user/.../mydata`.
+    Returns:
+        Store the absolute path of the data directory, is `root/chinese_lexicon_animal`.
+    """
+    start = time.time()
+    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
+    task_path = os.path.join(root, 'chinese_lexicon_animal')
+    if tf.gfile.Exists(task_path):
+        tf.gfile.DeleteRecursively(task_path)
+    tf.gfile.MakeDirs(task_path)
+    url = "https://raw.githubusercontent.com/Hourout/datasets/master/nlp/chinese_lexicon/chinese_lexicon_animal.txt"
+    data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)
+    data.to_csv(os.path.join(task_path, 'chinese_lexicon_animal.txt'), index=False, header=None)
+    print('chinese_lexicon_animal dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return task_path
+
+def medical(root):
+    """Chinese lexicon medical datasets.
+    
+    datasets url:`https://github.com/fighting41love/funNLP`
+    
+    chinese_lexicon_medical dataset contains 18700+ samples.
+    
+    Data storage directory:
+    root = `/user/.../mydata`
+    chinese_lexicon_medical data: 
+    `root/chinese_lexicon_medical/chinese_lexicon_medical.txt`
+    Args:
+        root: str, Store the absolute path of the data directory.
+              example:if you want data path is `/user/.../mydata/chinese_lexicon_medical`,
+              root should be `/user/.../mydata`.
+    Returns:
+        Store the absolute path of the data directory, is `root/chinese_lexicon_medical`.
+    """
+    start = time.time()
+    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
+    task_path = os.path.join(root, 'chinese_lexicon_medical')
+    if tf.gfile.Exists(task_path):
+        tf.gfile.DeleteRecursively(task_path)
+    tf.gfile.MakeDirs(task_path)
+    url = "https://raw.githubusercontent.com/Hourout/datasets/master/nlp/chinese_lexicon/chinese_lexicon_medical.txt"
+    data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)
+    data.to_csv(os.path.join(task_path, 'chinese_lexicon_medical.txt'), index=False, header=None)
+    print('chinese_lexicon_medical dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return task_path
+
+def famous_person(root):
+    """Chinese lexicon famous person datasets.
+    
+    datasets url:`https://github.com/fighting41love/funNLP`
+    
+    chinese_lexicon_famous_person dataset contains 13600+ samples.
+    
+    Data storage directory:
+    root = `/user/.../mydata`
+    chinese_lexicon_famous_person data: 
+    `root/chinese_lexicon_famous_person/chinese_lexicon_famous_person.txt`
+    Args:
+        root: str, Store the absolute path of the data directory.
+              example:if you want data path is `/user/.../mydata/chinese_lexicon_famous_person`,
+              root should be `/user/.../mydata`.
+    Returns:
+        Store the absolute path of the data directory, is `root/chinese_lexicon_famous_person`.
+    """
+    start = time.time()
+    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
+    task_path = os.path.join(root, 'chinese_lexicon_famous_person')
+    if tf.gfile.Exists(task_path):
+        tf.gfile.DeleteRecursively(task_path)
+    tf.gfile.MakeDirs(task_path)
+    url = "https://raw.githubusercontent.com/Hourout/datasets/master/nlp/chinese_lexicon/chinese_lexicon_famous_person.txt"
+    data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None, sep='\t')[0]
+    data.to_csv(os.path.join(task_path, 'chinese_lexicon_famous_person.txt'), index=False, header=None)
+    print('chinese_lexicon_famous_person dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
