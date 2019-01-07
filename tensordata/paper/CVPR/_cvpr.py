9a4840eac5f4dbaeac5a224bab492c63a4f4db40
JinQing Lee <hourout@163.com>
2019-01-07 18:13:30 +0800

    Update and rename tensordata/paper/_paper.py to tensordata/paper/CVPR/_cvpr.py

--- tensordata/paper/CVPR/_cvpr.py
+++ tensordata/paper/CVPR/_cvpr.py
@@ -7,8 +7,7 @@ import pandas as pd
 import tensorflow as tf
 
 
-__all__ = ['arxiv', 'CVPR2018', 'CVPR2017', 'CVPR2016', 'CVPR2015', 'CVPR2014', 'CVPR2013',
-           'ICCV2013', 'ICCV2015', 'ICCV2017'
+__all__ = ['CVPR2018', 'CVPR2017', 'CVPR2016', 'CVPR2015', 'CVPR2014', 'CVPR2013',
           ]
 
 def _download(path):
@@ -216,70 +215,4 @@ def CVPR2013(root):
     _get_paper('CVPR2013', root,
                url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2013.txt')
     print('CVPR2013 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
-    return task_path
-
-def ICCV2017(root):
-    """ICCV2017 datasets from http://openaccess.thecvf.com/ICCV2017.py.
-        
-    ICCV2017 datasets includes 621 papers.
-    
-    Data storage directory:
-    root = `/user/.../mydata`
-    ICCV2017 data: 
-    `root/ICCV2017/...`
-    Args:
-        root: str, Store the absolute path of the data directory.
-              example:if you want data path is `/user/.../mydata/ICCV2017`,
-              root should be `/user/.../mydata`.
-    Returns:
-        Store the absolute path of the data directory, is `root/ICCV2017`.
-    """
-    start = time.time()
-    _get_paper('ICCV2017', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/ICCV/iccv2017.txt')
-    print('ICCV2017 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
-    return task_path
-
-def ICCV2015(root):
-    """ICCV2015 datasets from http://openaccess.thecvf.com/ICCV2015.py.
-        
-    ICCV2015 datasets includes 526 papers.
-    
-    Data storage directory:
-    root = `/user/.../mydata`
-    ICCV2015 data: 
-    `root/ICCV2015/...`
-    Args:
-        root: str, Store the absolute path of the data directory.
-              example:if you want data path is `/user/.../mydata/ICCV2015`,
-              root should be `/user/.../mydata`.
-    Returns:
-        Store the absolute path of the data directory, is `root/ICCV2015`.
-    """
-    start = time.time()
-    _get_paper('ICCV2015', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/ICCV/iccv2015.txt')
-    print('ICCV2015 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
-    return task_path
-
-def ICCV2013(root):
-    """ICCV2013 datasets from http://openaccess.thecvf.com/ICCV2013.py.
-        
-    ICCV2013 datasets includes 455 papers.
-    
-    Data storage directory:
-    root = `/user/.../mydata`
-    ICCV2013 data: 
-    `root/ICCV2013/...`
-    Args:
-        root: str, Store the absolute path of the data directory.
-              example:if you want data path is `/user/.../mydata/ICCV2013`,
-              root should be `/user/.../mydata`.
-    Returns:
-        Store the absolute path of the data directory, is `root/ICCV2013`.
-    """
-    start = time.time()
-    _get_paper('ICCV2013', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/ICCV/iccv2013.txt')
-    print('ICCV2013 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
