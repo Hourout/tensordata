fa6807d121ee9d5abafada7e99d10dc4bf661644
JinQing Lee <hourout@163.com>
2019-01-09 18:11:06 +0800

    Update _lexicon.py

--- tensordata/nlp/chinese/lexicon/_lexicon.py
+++ tensordata/nlp/chinese/lexicon/_lexicon.py
@@ -6,7 +6,7 @@ import pandas as pd
 import tensorflow as tf
 
 __all__ = ['it', 'animal', 'medical', 'famous_person', 'placename', 'antonym', 
-           'synonym', 'privative', 'idiom', 'pornography', 
+           'synonym', 'privative', 'idiom', 'pornography', 'car',
 ]
 
 def it(root):
@@ -307,4 +307,34 @@ def pornography(root):
     data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)
     data.to_csv(os.path.join(task_path, 'chinese_lexicon_pornography.txt'), index=False, header=None)
     print('chinese_lexicon_pornography dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return task_path
+
+def car(root):
+    """Chinese lexicon car datasets.
+    
+    datasets url:`https://github.com/fighting41love/funNLP`
+    
+    chinese_lexicon_car dataset contains 3700+ samples.
+    
+    Data storage directory:
+    root = `/user/.../mydata`
+    chinese_lexicon_car data: 
+    `root/chinese_lexicon_car/chinese_lexicon_car.txt`
+    Args:
+        root: str, Store the absolute path of the data directory.
+              example:if you want data path is `/user/.../mydata/chinese_lexicon_car`,
+              root should be `/user/.../mydata`.
+    Returns:
+        Store the absolute path of the data directory, is `root/chinese_lexicon_car`.
+    """
+    start = time.time()
+    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
+    task_path = os.path.join(root, 'chinese_lexicon_car')
+    if tf.gfile.Exists(task_path):
+        tf.gfile.DeleteRecursively(task_path)
+    tf.gfile.MakeDirs(task_path)
+    url = "https://raw.githubusercontent.com/Hourout/datasets/master/nlp/chinese_lexicon/chinese_lexicon_car.txt"
+    data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)
+    data.to_csv(os.path.join(task_path, 'chinese_lexicon_car.txt'), index=False, header=None)
+    print('chinese_lexicon_car dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
