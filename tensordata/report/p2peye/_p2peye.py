f4b0efa6c93631c659c2b46e9fd95e752b33a3a5
JinQing Lee <hourout@163.com>
2019-01-18 18:57:21 +0800

    Update _p2peye.py

--- tensordata/report/p2peye/_p2peye.py
+++ tensordata/report/p2peye/_p2peye.py
@@ -3,7 +3,7 @@ import time
 import requests
 import pandas as pd
 
-__all__ =['rating']
+__all__ =['rating', 'problem_platform']
 
 def rating(date=None):
     """P2peye comprehensive rating and display results.
@@ -23,4 +23,28 @@ def rating(date=None):
     s = requests.get(url_txt).content
     data = pd.read_csv(io.StringIO(s.decode('utf-8')))
     print('p2peye rating dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
+    return data
+
+def problem_platform(date=None, days=7):
+    """P2peye problem platform and display results.
+    
+    from https://www.p2peye.com
+    
+    Args:
+        date: if None, download latest data, if like '2018-12-01', that download month data.
+        days: latest (date-days) day data.
+    Returns:
+        DataFrame
+    """
+    start = time.time()
+    if date is None:
+        date = str(pd.to_datetime(pd.datetime.now()).floor('d'))[:10]
+    assert (isinstance(date, str) and len(date)==10), "`date` shoule format '2018-12-01' or None"
+    date = pd.to_datetime(date)
+    update = date-pd.DateOffset(days=days)
+    url_txt = 'https://raw.githubusercontent.com/Hourout/datasets/master/report/p2peye/problem_platform/problem_platform.txt'
+    s = requests.get(url_txt).content
+    data = pd.read_csv(io.StringIO(s.decode('utf-8')), parse_dates=['问题发生时间'])
+    data = data[(data['问题发生时间']<=date)&(data['问题发生时间']>update)].reset_index(drop=True)
+    print('p2peye problem platform dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return data
