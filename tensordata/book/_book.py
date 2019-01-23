2a1ba193ba6d8868371582c17ed7a9663a9cf4e0
JinQing Lee <hourout@163.com>
2019-01-23 14:11:04 +0800

    Update _book.py

--- tensordata/book/_book.py
+++ tensordata/book/_book.py
@@ -14,11 +14,16 @@ def economist(root, date, mode='pdf'):
     `root/...(pdf or epub or mobi)`
     Args:
         root: str, Store the absolute path of the data directory.
+        date: str, eg:'2019-01-01'.
+        mode: str, one of ['pdf', 'epub', 'mobi'].
     Returns:
         Store the absolute path of the data directory, is `root/...(pdf or epub or mobi)`.
     """
     start = time.time()
     assert mode in ['pdf', 'epub', 'mobi'], "`mode` should be one of ['pdf', 'epub', 'mobi']."
+    t = divmod((pd.to_datetime(date)-pd.to_datetime('2017-05-06')).days, 7)
+    if t[0]<0 or t[1]>0:
+        raise ValueError("No book that meets the date.") 
     task_path = assert_dirs(root)
     url = 'https://github.com/nailperry-zd/The-Economist/raw/master/'+date+'/The_Economist_-_'+date+'.'+mode
     task_path = os.path.join(task_path, url.split('/')[-1])
