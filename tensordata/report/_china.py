38082af3be0f8b814fe062eed0314268cb6bd48b
JinQing Lee <hourout@163.com>
2019-01-24 18:38:07 +0800

    Update _china.py

--- tensordata/report/_china.py
+++ tensordata/report/_china.py
@@ -1,13 +1,20 @@
 import requests
 import pandas as pd
 
-__all__ = ['china_id_card', 'china_mobie']
+__all__ = ['china_id_card', 'china_mobie', 'logistics']
+
+company = {"申通":"shentong", "EMS":"ems", "顺丰":"shunfeng", "圆通":"yuantong",
+           "中通":"zhongtong", "韵达":"yunda", "天天":"tiantian", "汇通":"huitongkuaidi",
+           "全峰":"quanfengkuaidi", "德邦":"debangwuliu", "宅急送":"zhaijisong"}
 
 def _get_id_card(card):
     return requests.get("https://www.fesugar.com/get_identitycards?id="+card).json()
 
 def _get_mobie(mobile):
     return requests.get("http://mobsec-dianhua.baidu.com/dianhua_api/open/location?tel="+mobile).json()
+              
+def _get_logistics(com, card):
+    return requests.get("http://www.kuaidi100.com/query?type="+company[com]+"&postid="+card).json()
 
 def china_id_card(card):
     assert isinstance(card, str), "`card` should be str."
@@ -22,4 +29,16 @@ def china_mobie(mobile):
     assert isinstance(mobile, str), "`mobile` should be str."
     s = _get_mobie(mobile)
     s = s['response']
+    return s
+
+def logistics(com, card):
+    assert isinstance(com, str), "`com` should be str."
+    assert isinstance(card, str), "`card` should be str."
+    if com not in company:
+        raise ValueError("`com` is error.")
+    s = _get_logistics(com, card)
+    if s['message']!='ok':
+        s = s['message']
+    else:
+        s = pd.DataFrame(s['data'])
     return s
