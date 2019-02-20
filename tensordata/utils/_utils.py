6cfc895201e55c38e02967dcdad085a4a332f100
JinQing Lee <hourout@163.com>
2019-02-20 15:55:38 +0800

    Update _utils.py

--- tensordata/utils/_utils.py
+++ tensordata/utils/_utils.py
@@ -1,18 +1,19 @@
+import os
 import tensorflow as tf
 
 def assert_dirs(root, root_dir=None, delete=True):
-    assert tf.gfile.IsDirectory(root), '{} should be directory.'.format(root)
+    assert tf.io.gfile.isdir(root), '{} should be directory.'.format(root)
     if root_dir is not None:
         assert isinstance(root_dir, str), '{} should be str.'.format(root_dir)
         task_path = os.path.join(root, root_dir)
-        if tf.gfile.Exists(task_path):
+        if tf.io.gfile.exists(task_path):
             if delete:
-                tf.gfile.DeleteRecursively(task_path)
-                tf.gfile.MakeDirs(task_path)
+                tf.io.gfile.rmtree(task_path)
+                tf.io.gfile.makedirs(task_path)
         else:
-            tf.gfile.MakeDirs(task_path)
+            tf.io.gfile.makedirs(task_path)
         return task_path
     else:
-        if not tf.gfile.Exists(root):
-            tf.gfile.MakeDirs(root)
+        if not tf.io.gfile.exists(root):
+            tf.io.gfile.makedirs(root)
         return root
