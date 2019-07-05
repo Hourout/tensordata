4ba1641a3a720ccf7f97a6651eb6032c369a6131
JinQing Lee <hourout@163.com>
2019-07-05 11:26:54 +0800

    update to tf2.0

--- tensordata/utils/_utils.py
+++ tensordata/utils/_utils.py
@@ -1,19 +1,18 @@
-import os
-import tensorflow as tf
+from tensorflow.io import gfile
 
 def assert_dirs(root, root_dir=None, delete=True):
-    assert tf.io.gfile.isdir(root), '{} should be directory.'.format(root)
+    assert gfile.isdir(root), '{} should be directory.'.format(root)
     if root_dir is not None:
         assert isinstance(root_dir, str), '{} should be str.'.format(root_dir)
-        task_path = os.path.join(root, root_dir)
-        if tf.io.gfile.exists(task_path):
+        task_path = root+'/'+root_dir
+        if gfile.exists(task_path):
             if delete:
-                tf.io.gfile.rmtree(task_path)
-                tf.io.gfile.makedirs(task_path)
+                gfile.rmtree(task_path)
+                gfile.makedirs(task_path)
         else:
-            tf.io.gfile.makedirs(task_path)
+            gfile.makedirs(task_path)
         return task_path
     else:
-        if not tf.io.gfile.exists(root):
-            tf.io.gfile.makedirs(root)
+        if not gfile.exists(root):
+            gfile.makedirs(root)
         return root
