91bbb75a970f425bd2757fc271a8023ab461ca25
JinQing Lee <hourout@163.com>
2019-07-05 15:11:36 +0800

    update new func

--- tensordata/utils/_utils.py
+++ tensordata/utils/_utils.py
@@ -1,6 +1,6 @@
 from tensorflow.io import gfile
 
-def assert_dirs(root, root_dir=None, delete=True):
+def assert_dirs(root, root_dir=None, delete=True, make_root_dir=True):
     assert gfile.isdir(root), '{} should be directory.'.format(root)
     if root_dir is not None:
         assert isinstance(root_dir, str), '{} should be str.'.format(root_dir)
@@ -10,7 +10,8 @@ def assert_dirs(root, root_dir=None, delete=True):
                 gfile.rmtree(task_path)
                 gfile.makedirs(task_path)
         else:
-            gfile.makedirs(task_path)
+            if make_root_dir:
+                gfile.makedirs(task_path)
         return task_path
     else:
         if not gfile.exists(root):
