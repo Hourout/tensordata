406c4c9bc021c43257d813354101d8b89b8e405b
JinQing Lee <hourout@163.com>
2019-07-08 10:58:58 +0800

    update to tf2.0

--- tensordata/cv/_cifar.py
+++ tensordata/cv/_cifar.py
@@ -1,9 +1,10 @@
-import os
 import time
 import tarfile
 import imageio
 import numpy as np
-import tensorflow as tf
+from tensorflow.io import gfile
+from tensordata.utils._utils import assert_dirs, path_join
+import tensordata.utils.request as rq
 
 __all__ = ['cifar10', 'cifar100']
 
@@ -30,36 +31,32 @@ def cifar10(root):
         Store the absolute path of the data directory, is `root/cifar10`.
     """
     start = time.time()
-    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-    task_path = os.path.join(root, 'cifar10')
+    task_path = assert_dirs(root, 'cifar10')
     url = 'https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/cifar10/cifar-10-binary.tar.gz'
-    if tf.gfile.Exists(task_path):
-        tf.gfile.DeleteRecursively(task_path)
-    tf.gfile.MakeDirs(task_path)
-    tf.keras.utils.get_file(os.path.join(task_path, url.split('/')[-1]), url)
-    with tarfile.open(os.path.join(task_path, url.split('/')[-1])) as t:
+    rq.files(url, path_join(task_path, url.split('/')[-1]))
+    with tarfile.open(path_join(task_path, url.split('/')[-1])) as t:
         t.extractall(task_path)
-    noise_flie = tf.gfile.ListDirectory(task_path)
+    noise_flie = gfile.listdir(task_path)
     for file in ['data_batch_1.bin', 'data_batch_2.bin', 'data_batch_3.bin', 'data_batch_4.bin', 'data_batch_5.bin']:
-        with open(os.path.join(task_path, file), 'rb') as fin:
+        with open(path_join(task_path, file), 'rb') as fin:
             data = np.frombuffer(fin.read(), dtype=np.uint8).reshape(-1, 3072+1)
             train = data[:, 1:].reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
             train_label = data[:, 0].astype(np.int32)
         for i in set(train_label):
-            tf.gfile.MakeDirs(os.path.join(task_path, 'train', str(i)))
+            gfile.makedirs(path_join(task_path, 'train', str(i)))
         for idx in range(train.shape[0]):
-            imageio.imsave(os.path.join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
+            imageio.imsave(path_join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
     for file in ['test_batch.bin']:
-        with open(os.path.join(task_path, file), 'rb') as fin:
+        with open(path_join(task_path, file), 'rb') as fin:
             data = np.frombuffer(fin.read(), dtype=np.uint8).reshape(-1, 3072+1)
             test = data[:, 1:].reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
             test_label = data[:, 0].astype(np.int32)
         for i in set(test_label):
-            tf.gfile.MakeDirs(os.path.join(task_path, 'test', str(i)))
+            gfile.makedirs(path_join(task_path, 'test', str(i)))
         for idx in range(test.shape[0]):
-            imageio.imsave(os.path.join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
+            imageio.imsave(path_join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
     for file in noise_flie:
-        tf.gfile.Remove(os.path.join(task_path, file))
+        gfile.remove(path_join(task_path, file))
     print('cifar10 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
 
@@ -89,33 +86,29 @@ def cifar100(root, fine_label=True):
         Store the absolute path of the data directory, is `root/cifar100`.
     """
     start = time.time()
-    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-    task_path = os.path.join(root, 'cifar100')
+    task_path = assert_dirs(root, 'cifar100')
     url = 'https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/cifar100/cifar-100-binary.tar.gz'
-    if tf.gfile.Exists(task_path):
-        tf.gfile.DeleteRecursively(task_path)
-    tf.gfile.MakeDirs(task_path)
-    tf.keras.utils.get_file(os.path.join(task_path, url.split('/')[-1]), url)
-    with tarfile.open(os.path.join(task_path, url.split('/')[-1])) as t:
+    rq.files(url, path_join(task_path, url.split('/')[-1]))
+    with tarfile.open(path_join(task_path, url.split('/')[-1])) as t:
         t.extractall(task_path)
-    noise_flie = tf.gfile.ListDirectory(task_path)
-    with open(os.path.join(task_path, 'train.bin'), 'rb') as fin:
+    noise_flie = gfile.listdir(task_path)
+    with open(path_join(task_path, 'train.bin'), 'rb') as fin:
         data = np.frombuffer(fin.read(), dtype=np.uint8).reshape(-1, 3072+2)
         train = data[:, 2:].reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
         train_label = data[:, 0+fine_label].astype(np.int32)
     for i in set(train_label):
-        tf.gfile.MakeDirs(os.path.join(task_path, 'train', str(i)))
+        gfile.makedirs(path_join(task_path, 'train', str(i)))
     for idx in range(train.shape[0]):
-        imageio.imsave(os.path.join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
-    with open(os.path.join(task_path, 'test.bin'), 'rb') as fin:
+        imageio.imsave(path_join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
+    with open(path_join(task_path, 'test.bin'), 'rb') as fin:
         data = np.frombuffer(fin.read(), dtype=np.uint8).reshape(-1, 3072+2)
         test = data[:, 2:].reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
         test_label = data[:, 0+fine_label].astype(np.int32)
     for i in set(test_label):
-        tf.gfile.MakeDirs(os.path.join(task_path, 'test', str(i)))
+        gfile.makedirs(path_join(task_path, 'test', str(i)))
     for idx in range(test.shape[0]):
-        imageio.imsave(os.path.join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
+        imageio.imsave(path_join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
     for file in noise_flie:
-        tf.gfile.Remove(os.path.join(task_path, file))
+        gfile.remove(path_join(task_path, file))
     print('cifar100 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
