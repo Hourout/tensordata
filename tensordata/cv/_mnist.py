baf8bc0d5e068f7698beccc98396f75e9ce28996
JinQing Lee <hourout@163.com>
2019-07-08 10:36:44 +0800

    update to tf2.0

--- tensordata/cv/_mnist.py
+++ tensordata/cv/_mnist.py
@@ -3,7 +3,9 @@ import gzip
 import time
 import imageio
 import numpy as np
-import tensorflow as tf
+from tensorflow.io import gfile
+from tensordata.utils._utils import assert_dirs, path_join
+import tensordata.utils.request as rq
 
 __all__ = ['mnist', 'mnist_fashion']
 
@@ -30,39 +32,35 @@ def mnist(root):
         Store the absolute path of the data directory, is `root/mnist`.
     """
     start = time.time()
-    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-    task_path = os.path.join(root, 'mnist')
+    task_path = assert_dirs(root, 'mnist')
     url_list = ['https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/mnist/train-labels-idx1-ubyte.gz',
                 'https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/mnist/train-images-idx3-ubyte.gz',
                 'https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/mnist/t10k-labels-idx1-ubyte.gz',
                 'https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/mnist/t10k-images-idx3-ubyte.gz']
-    if tf.gfile.Exists(task_path):
-        tf.gfile.DeleteRecursively(task_path)
-    tf.gfile.MakeDirs(task_path)
     for url in url_list:
-        tf.keras.utils.get_file(os.path.join(task_path, url.split('/')[-1]), url)
-    with gzip.open(os.path.join(task_path, 'train-labels-idx1-ubyte.gz'), 'rb') as lbpath:
+        rq.files(url, path_join(task_path, url.split('/')[-1]))
+    with gzip.open(path_join(task_path, 'train-labels-idx1-ubyte.gz'), 'rb') as lbpath:
         train_label = np.frombuffer(lbpath.read(), np.uint8, offset=8)
 
-    with gzip.open(os.path.join(task_path, 'train-images-idx3-ubyte.gz'), 'rb') as imgpath:
+    with gzip.open(path_join(task_path, 'train-images-idx3-ubyte.gz'), 'rb') as imgpath:
         train = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(train_label), 28, 28)
 
-    with gzip.open(os.path.join(task_path, 't10k-labels-idx1-ubyte.gz'), 'rb') as lbpath:
+    with gzip.open(path_join(task_path, 't10k-labels-idx1-ubyte.gz'), 'rb') as lbpath:
         test_label = np.frombuffer(lbpath.read(), np.uint8, offset=8)
 
-    with gzip.open(os.path.join(task_path, 't10k-images-idx3-ubyte.gz'), 'rb') as imgpath:
+    with gzip.open(path_join(task_path, 't10k-images-idx3-ubyte.gz'), 'rb') as imgpath:
         test = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(test_label), 28, 28)
     
     for i in set(train_label):
-        tf.gfile.MakeDirs(os.path.join(task_path, 'train', str(i)))
+        gfile.makedirs(path_join(task_path, 'train', str(i)))
     for i in set(test_label):
-        tf.gfile.MakeDirs(os.path.join(task_path, 'test', str(i)))
+        gfile.makedirs(path_join(task_path, 'test', str(i)))
     for idx in range(train.shape[0]):
-        imageio.imsave(os.path.join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
+        imageio.imsave(path_join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
     for idx in range(test.shape[0]):
-        imageio.imsave(os.path.join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
+        imageio.imsave(path_join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
     for url in url_list:
-        tf.gfile.Remove(os.path.join(task_path, url.split('/')[-1]))
+        gfile.remove(path_join(task_path, url.split('/')[-1]))
     print('mnist dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
 
@@ -91,38 +89,34 @@ def mnist_fashion(root):
         Store the absolute path of the data directory, is `root/mnist_fashion`.
     """
     start = time.time()
-    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-    task_path = os.path.join(root, 'mnist_fashion')
+    task_path = assert_dirs(root, 'mnist_fashion')
     url_list = ['http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz',
                 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz',
                 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz',
                 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz']
-    if tf.gfile.Exists(task_path):
-        tf.gfile.DeleteRecursively(task_path)
-    tf.gfile.MakeDirs(task_path)
     for url in url_list:
-        tf.keras.utils.get_file(os.path.join(task_path, url.split('/')[-1]), url)
-    with gzip.open(os.path.join(task_path, 'train-labels-idx1-ubyte.gz'), 'rb') as lbpath:
+        rq.files(url, path_join(task_path, url.split('/')[-1]))
+    with gzip.open(path_join(task_path, 'train-labels-idx1-ubyte.gz'), 'rb') as lbpath:
         train_label = np.frombuffer(lbpath.read(), np.uint8, offset=8)
 
-    with gzip.open(os.path.join(task_path, 'train-images-idx3-ubyte.gz'), 'rb') as imgpath:
+    with gzip.open(path_join(task_path, 'train-images-idx3-ubyte.gz'), 'rb') as imgpath:
         train = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(train_label), 28, 28)
 
-    with gzip.open(os.path.join(task_path, 't10k-labels-idx1-ubyte.gz'), 'rb') as lbpath:
+    with gzip.open(path_join(task_path, 't10k-labels-idx1-ubyte.gz'), 'rb') as lbpath:
         test_label = np.frombuffer(lbpath.read(), np.uint8, offset=8)
 
-    with gzip.open(os.path.join(task_path, 't10k-images-idx3-ubyte.gz'), 'rb') as imgpath:
+    with gzip.open(path_join(task_path, 't10k-images-idx3-ubyte.gz'), 'rb') as imgpath:
         test = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(test_label), 28, 28)
     
     for i in set(train_label):
-        tf.gfile.MakeDirs(os.path.join(task_path, 'train', str(i)))
+        gfile.makedirs(path_join(task_path, 'train', str(i)))
     for i in set(test_label):
-        tf.gfile.MakeDirs(os.path.join(task_path, 'test', str(i)))
+        gfile.makedirs(path_join(task_path, 'test', str(i)))
     for idx in range(train.shape[0]):
-        imageio.imsave(os.path.join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
+        imageio.imsave(path_join(task_path, 'train', str(train_label[idx]), str(idx)+'.png'), train[idx])
     for idx in range(test.shape[0]):
-        imageio.imsave(os.path.join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
+        imageio.imsave(path_join(task_path, 'test', str(test_label[idx]), str(idx)+'.png'), test[idx])
     for url in url_list:
-        tf.gfile.Remove(os.path.join(task_path, url.split('/')[-1]))
+        gfile.remove(path_join(task_path, url.split('/')[-1]))
     print('mnist_fashion dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
