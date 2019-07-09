2dee44466e5a5f3fbea5b7f7bd29eaad30588bf1
JinQing Lee <hourout@163.com>
2019-07-09 11:20:58 +0800

    add new func

--- tensordata/utils/_utils.py
+++ tensordata/utils/_utils.py
@@ -30,6 +30,7 @@ def _download(path):
     rq.files(path.split('|')[1], path_join(path.split('|')[0], path.split('|')[1].split('/')[-1]))
     
 def get_paper(name, root, url):
+    task_path = assert_dirs(root, name)
     data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)[0].tolist()
     data = [task_path+'|'+i for i in data]
     with concurrent.futures.ProcessPoolExecutor() as excutor:
