41df875846822264795b66f9b40bf05fd61be6f9
JinQing Lee <hourout@163.com>
2019-07-09 14:17:26 +0800

    update to tf2.0

--- tensordata/utils/compress/_folder_compress.py
+++ tensordata/utils/compress/_folder_compress.py
@@ -4,7 +4,8 @@ import gzip
 import zipfile
 import tarfile
 import rarfile
-import tensorflow as tf
+from tensorflow.io import gfile
+from tensordata.utils._utils import path_join
 
 __all__ = ['folder_zip', 'folder_tar']
 
@@ -17,13 +18,13 @@ def folder_zip(folder, zip_name):
     Return:
         zip_name: str, compression files name.
     """
-    assert tf.gfile.IsDirectory(folder), '`folder` should be folder path.'
+    assert gfile.isdir(folder), '`folder` should be folder path.'
     with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as z:
-        for dirpath, dirnames, filenames in tf.gfile.Walk(folder):
+        for dirpath, dirnames, filenames in gfile.walk(folder):
             fpath = dirpath.replace(startdir, '')
             fpath = fpath and fpath + os.sep or ''
             for filename in filenames:
-                z.write(os.path.join(dirpath, filename), fpath+filename)
+                z.write(path_join(dirpath, filename), path_join(fpath, filename))
     return zip_name
 
 def folder_tar(folder, tar_name):
@@ -35,11 +36,11 @@ def folder_tar(folder, tar_name):
     Return:
         zip_name: str, compression files name.
     """
-    assert tf.gfile.IsDirectory(folder), '`folder` should be folder path.'
+    assert gfile.isdir(folder), '`folder` should be folder path.'
     with tarfile.open(tar_name, 'w') as tar:
-        for dirpath, dirnames, filenames in tf.gfile.Walk(tar_name):
+        for dirpath, dirnames, filenames in gfile.walk(tar_name):
             fpath = dirpath.replace(startdir, '')
             fpath = fpath and fpath + os.sep or ''
             for filename in filenames:
-                tar.add(os.path.join(dirpath, filename), fpath+filename)
+                tar.add(path_join(dirpath, filename), path_join(fpath, filename))
     return tar_name
