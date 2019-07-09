01a2c5635f67ac57986883c595c8b4ddba2a3e1c
JinQing Lee <hourout@163.com>
2019-07-09 14:09:29 +0800

    update to tf2.0

--- tensordata/utils/compress/_files_compress.py
+++ tensordata/utils/compress/_files_compress.py
@@ -1,10 +1,9 @@
-import os
 import bz2
 import gzip
 import zipfile
 import tarfile
 import rarfile
-import tensorflow as tf
+from tensorflow.io import gfile
 
 __all__ = ['files_zip', 'files_tar', 'files_bz2']
 
@@ -21,13 +20,13 @@ def files_zip(files, zip_name):
     """
     assert isinstance(zip_name, str), '`zip_name` should be str.'
     if isinstance(files, str):
-        assert not tf.gfile.IsDirectory(files), '`files` should be file path.'
+        assert not gfile.isdir(files), '`files` should be file path.'
         with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as z:
             z.write(files)
     elif isinstance(files, list):
         with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as z:
             for file in files:
-                assert not tf.gfile.IsDirectory(file), 'Elements in the list should be file path.'
+                assert not gfile.isdir(file), 'Elements in the list should be file path.'
                 z.write(file)
     else:
         raise ValueError('`files` should be type of str or list.')
@@ -46,13 +45,13 @@ def files_tar(files, tar_name):
     """
     assert isinstance(tar_name, str), '`tar_name` should be str.'
     if isinstance(files, str):
-        assert not tf.gfile.IsDirectory(files), '`files` should be file path.'
+        assert not gfile.isdir(files), '`files` should be file path.'
         with tarfile.TarFile(tar_name, 'w') as t:
             t.add(files)
     elif isinstance(files, list):
         with tarfile.TarFile(tar_name, 'w') as t:
             for file in files:
-                assert not tf.gfile.IsDirectory(file), 'Elements in the list should be file path.'
+                assert not gfile.isdir(file), 'Elements in the list should be file path.'
                 t.add(file)
     else:
         raise ValueError('`files` should be type of str or list.')
@@ -71,15 +70,15 @@ def files_bz2(files, bz2_name):
     """
     assert isinstance(bz2_name, str), '`bz2_name` should be str.'
     if isinstance(files, str):
-        assert not tf.gfile.IsDirectory(files), '`files` should be file path.'
+        assert not gfile.isdir(files), '`files` should be file path.'
         with bz2.BZ2File(bz2_name, 'w') as b:
-            with tf.gfile.GFile(files, 'rb') as f:
+            with gfile.GFile(files, 'rb') as f:
                 b.write(f.read())
     elif isinstance(files, list):
         with bz2.BZ2File(bz2_name, 'w') as b:
             for file in files:
-                assert not tf.gfile.IsDirectory(file), 'Elements in the list should be file path.'
-                with tf.gfile.GFile(file, 'rb') as f:
+                assert not gfile.isdir(file), 'Elements in the list should be file path.'
+                with gfile.GFile(file, 'rb') as f:
                     b.write(f.read())
     else:
         raise ValueError('`files` should be type of str or list.')
