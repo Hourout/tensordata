2ebfc284f296a826a6de461348d7c653d6f63643
JinQing Lee <hourout@163.com>
2019-07-09 14:25:56 +0800

    Update __init__.py

--- tensordata/__init__.py
+++ tensordata/__init__.py
@@ -6,6 +6,6 @@ from tensordata import paper
 from tensordata import report
 from tensordata import book
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'JinQing Lee'
 __data_version__ = '0.1.0'
