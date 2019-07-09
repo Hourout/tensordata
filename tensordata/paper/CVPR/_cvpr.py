d96279d635328bb83efc2e29d7b8b26e293ca01d
JinQing Lee <hourout@163.com>
2019-07-09 11:26:56 +0800

    update to tf2.0

--- tensordata/paper/CVPR/_cvpr.py
+++ tensordata/paper/CVPR/_cvpr.py
@@ -1,62 +1,9 @@
-import os
-import io
 import time
-import requests
-import concurrent
-import pandas as pd
-import tensorflow as tf
+from tensordata.utils._utils import get_paper
 
 
 __all__ = ['CVPR2018', 'CVPR2017', 'CVPR2016', 'CVPR2015', 'CVPR2014', 'CVPR2013',
           ]
-
-def _download(path):
-    tf.keras.utils.get_file(os.path.join(path.split('|')[0], path.split('|')[1].split('/')[-1]), path.split('|')[1])
-
-def arxiv(root, ids, new_name=None):
-    """Download paper from https://arxiv.org, the file format is pdf.
-    
-    Data storage directory:
-    root = `/user/.../mydata`
-    `ids`.pdf data: 
-    `root/arxiv/`ids`.pdf` or `root/arxiv/`new_name`.pdf`
-    Args:
-        root: str, Store the absolute path of the data directory.
-              example:if you want data path is `/user/.../mydata/arxiv`,
-              root should be `/user/.../mydata`.
-        ids: str, arxiv paper id.
-             example:ids = '1605.09782' mean you want get paper links https://arxiv.org/abs/1605.09782.
-        new_name: str, default None. if not None, download file path is `root/arxiv/new_name.pdf`.
-    Returns:
-        Store the absolute path of the data directory, is `root/arxiv`.
-    """
-    start = time.time()
-    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-    assert isinstance(ids, str), '`ids` type should be str.'
-    assert isinstance(new_name, str), '`new_name` type should be str.'
-    if new_name is None:
-        task_path = os.path.join(root, 'arxiv', ids+'.pdf')
-    else:
-        task_path = os.path.join(root, 'arxiv', new_name+'.pdf')
-    if not tf.gfile.Exists(task_path):
-        tf.gfile.MakeDirs(task_path)
-    tf.gfile.DeleteRecursively(task_path)
-    url = 'https://arxiv.org/pdf/'+str(ids)+'.pdf'
-    tf.keras.utils.get_file(task_path, url)
-    print('arxiv paper download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
-    return task_path
-
-def _get_paper(name, root, url):
-    assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-    task_path = os.path.join(root, name)
-    if tf.gfile.Exists(task_path):
-        tf.gfile.DeleteRecursively(task_path)
-    tf.gfile.MakeDirs(task_path)
-    data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)[0].tolist()
-    data = [task_path+'|'+i for i in data]
-    with concurrent.futures.ProcessPoolExecutor() as excutor:
-        excutor.map(_download, data)
-    return task_path
 
 def CVPR2018(root):
     """CVPR2018 datasets from http://openaccess.thecvf.com/CVPR2018.py.
@@ -75,17 +22,7 @@ def CVPR2018(root):
         Store the absolute path of the data directory, is `root/CVPR2018`.
     """
     start = time.time()
-    _get_paper('CVPR2018', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2018.txt')
-#     assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-#     task_path = os.path.join(root, 'cvpr2018')
-#     if tf.gfile.Exists(task_path):
-#         tf.gfile.DeleteRecursively(task_path)
-#     tf.gfile.MakeDirs(task_path)
-#     url = 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2018.txt'
-#     data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)[0].tolist()
-#     with concurrent.futures.ProcessPoolExecutor() as excutor:
-#         excutor.map(_download, [task_path+'|'+i for i in data])
+    get_paper('CVPR2018', root, 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2018.txt')
     print('CVPR2018 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
 
@@ -106,17 +43,7 @@ def CVPR2017(root):
         Store the absolute path of the data directory, is `root/CVPR2017`.
     """
     start = time.time()
-    _get_paper('CVPR2017', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2017.txt')
-#     assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-#     task_path = os.path.join(root, 'cvpr2017')
-#     if tf.gfile.Exists(task_path):
-#         tf.gfile.DeleteRecursively(task_path)
-#     tf.gfile.MakeDirs(task_path)
-#     url = 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2017.txt'
-#     data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)[0].tolist()
-#     with concurrent.futures.ProcessPoolExecutor() as excutor:
-#         excutor.map(_download, [task_path+'|'+i for i in data])
+    get_paper('CVPR2017', root, 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2017.txt')
     print('CVPR2017 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
 
@@ -137,17 +64,7 @@ def CVPR2016(root):
         Store the absolute path of the data directory, is `root/CVPR2016`.
     """
     start = time.time()
-    _get_paper('CVPR2016', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2016.txt')
-#     assert tf.gfile.IsDirectory(root), '`root` should be directory.'
-#     task_path = os.path.join(root, 'cvpr2017')
-#     if tf.gfile.Exists(task_path):
-#         tf.gfile.DeleteRecursively(task_path)
-#     tf.gfile.MakeDirs(task_path)
-#     url = 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2017.txt'
-#     data = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), header=None)[0].tolist()
-#     with concurrent.futures.ProcessPoolExecutor() as excutor:
-#         excutor.map(_download, [task_path+'|'+i for i in data])
+    get_paper('CVPR2016', root, 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2016.txt')
     print('CVPR2016 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
 
@@ -168,8 +85,7 @@ def CVPR2015(root):
         Store the absolute path of the data directory, is `root/CVPR2015`.
     """
     start = time.time()
-    _get_paper('CVPR2015', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2015.txt')
+    get_paper('CVPR2015', root, 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2015.txt')
     print('CVPR2015 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
 
@@ -190,8 +106,7 @@ def CVPR2014(root):
         Store the absolute path of the data directory, is `root/CVPR2014`.
     """
     start = time.time()
-    _get_paper('CVPR2014', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2014.txt')
+    get_paper('CVPR2014', root, 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2014.txt')
     print('CVPR2014 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
 
@@ -212,7 +127,6 @@ def CVPR2013(root):
         Store the absolute path of the data directory, is `root/CVPR2013`.
     """
     start = time.time()
-    _get_paper('CVPR2013', root,
-               url='https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2013.txt')
+    get_paper('CVPR2013', root, 'https://raw.githubusercontent.com/Hourout/datasets/master/paper/cvpr/cvpr2013.txt')
     print('CVPR2013 dataset download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
     return task_path
