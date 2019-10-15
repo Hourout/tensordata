e66b5a4a9c89f489aee5cfca5019eef3a2fd6f9d
JinQing Lee <hourout@163.com>
2019-10-15 17:00:59 +0800

    fix tf.io import bug

--- tensordata/paper/_arxiv.py
+++ tensordata/paper/_arxiv.py
@@ -1,5 +1,5 @@
 import time
-from tensorflow.io import gfile
+import tensorflow as tf
 from tensordata.utils._utils import path_join
 import tensordata.utils.request as rq
 
@@ -23,17 +23,17 @@ def arxiv(root, ids, new_name=None):
         Store the absolute path of the data directory, is `root/arxiv`.
     """
     start = time.time()
-    assert gfile.isdir(root), '`root` should be directory.'
+    assert tf.io.gfile.isdir(root), '`root` should be directory.'
     assert isinstance(ids, str), '`ids` type should be str.'
     if new_name is None:
         task_path = path_join(root, 'arxiv', ids+'.pdf')
     else:
         task_path = path_join(root, 'arxiv', new_name+'.pdf')
-    if not gfile.exists(path_join(root, 'arxiv')):
-        gfile.makedirs(path_join(root, 'arxiv'))
-    if gfile.exists(task_path):
-        if not gfile.isdir(task_path):
-            gfile.remove(task_path)
+    if not tf.io.gfile.exists(path_join(root, 'arxiv')):
+        tf.io.gfile.makedirs(path_join(root, 'arxiv'))
+    if tf.io.gfile.exists(task_path):
+        if not tf.io.gfile.isdir(task_path):
+            tf.io.gfile.remove(task_path)
     url = 'https://arxiv.org/pdf/'+str(ids)+'.pdf'
     rq.files(url, task_path)
     print('arxiv paper download completed, run time %d min %.2f sec' %divmod((time.time()-start), 60))
