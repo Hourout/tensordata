28f8e119783a7df9d9349455f1e2d7bf9df47533
JinQing Lee <hourout@163.com>
2019-10-15 16:29:17 +0800

    fix tf.io import bug

--- tensordata/utils/request/_download.py
+++ tensordata/utils/request/_download.py
@@ -2,8 +2,7 @@ import io
 import json as jsons
 import requests
 import pandas as pd
-from tensorflow.io import gfile
-from tensorflow.keras.utils import Progbar
+import tensorflow as tf
 
 __all__ = ['json', 'table', 'files']
 
@@ -18,7 +17,7 @@ def json(url, root_file):
     """
     assert root_file[-5:]=='.json', '`root_file` should be `xxx.json`'
     s = requests.get(url)
-    with gfile.GFile(root_file, 'w') as f:
+    with tf.io.gfile.GFile(root_file, 'w') as f:
         jsons.dump(s.json(), f, ensure_ascii=False)
     return root_file
 
@@ -51,7 +50,7 @@ def files(url, root_file, verbose=1, chunk_size=1024):
     r = requests.get(url, stream=True)
     content_type = r.headers.get('Content-Length')
     total_size = -1 if content_type is None else int(content_type.strip())
-    p = Progbar(total_size, verbose=verbose)
+    p = tf.keras.utils.Progbar(total_size, verbose=verbose)
     with open(root_file, 'wb') as f:
         for chunk in r.iter_content(chunk_size):
             p.add(chunk_size)
